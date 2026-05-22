832d8ea84b3fa15e449ebfcd95e3369c9e94c613
Marvin Cardona <marvincardona823@gmail.com>
2026-05-23 00:27:58 +0800

    Make database connection dynamic for Render

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import os
 from mysql.connector import pooling
 
 db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "stockwise_db",
-    "port": 3306,
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "user": os.environ.get("DB_USER", "root"),
+    "password": os.environ.get("DB_PASSWORD", ""),
+    "database": os.environ.get("DB_NAME", "stockwise_db"),
+    "port": int(os.environ.get("DB_PORT", 3306)),
 }
 
 connection_pool = pooling.MySQLConnectionPool(
